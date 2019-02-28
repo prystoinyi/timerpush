a86b730a830cabdd843d1c6d6a024e2c50190b82
Roman <roman.prystojnyj@gmail.com>
2019-02-28 22:25:26 +0200

    Changed the server to ASP.NET Core and updated the program structure in the client

--- repool-server/repool-server/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace repool_server.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
--- repool-server/repool-server/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace repool_server.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
