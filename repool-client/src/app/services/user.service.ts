b65c266fd4a28480a67ed00bfb686b9601f4cd8b
Roman <roman.prystojnyj@gmail.com>
2019-03-04 14:32:55 +0200

    Adding validation to the registration page.

--- repool-client/src/app/services/user.service.ts
+++ repool-client/src/app/services/user.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 
 @Injectable({
   providedIn: 'root'
@@ -9,12 +9,24 @@ export class UserService {
   constructor(private fb: FormBuilder) { }
 
   formModel = this.fb.group({
-    UserName: [''],
-    Email: [''],
+    UserName: ['', Validators.required],
+    Email: ['', Validators.email],
     FullName: [''],
     Passwords: this.fb.group({
-      Password: [''],
-      ConfirmPassword: ['']
-    })
+      Password: ['', [Validators.required, Validators.minLength(4)]],
+      ConfirmPassword: ['', Validators.required]
+    }, { validator: this.comparePasswords }), 
   });
+
+  comparePasswords(fb: FormGroup) {
+    let confirmPswrdCtrl = fb.get('ConfirmPassword');
+    //passwordMismatch
+    //confirmPswrdCtrl.errors={passwordMismatch:true}
+    if (confirmPswrdCtrl.errors == null || 'passwordMismatch' in confirmPswrdCtrl.errors) {
+      if (fb.get('Password').value != confirmPswrdCtrl.value)
+        confirmPswrdCtrl.setErrors({ passwordMismatch: true });
+      else
+        confirmPswrdCtrl.setErrors(null);
+    }
+  }
 }
