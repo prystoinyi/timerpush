5271e3fdfa8b4fde3a76df997217cc0acad0b906
Roman <roman.prystojnyj@gmail.com>
2019-07-15 23:40:49 +0300

    Added controller (Task)

---
+++ repool-server/repool-server/Controllers/TaskController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using repool_server.Models;
+
+namespace repool_server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TaskController : ControllerBase
+    {
+        private readonly AuthenticationContext _context;
+
+        public TaskController(AuthenticationContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Task
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TaskModel>>> GetTaskModels()
+        {
+            return await _context.TaskModels.ToListAsync();
+        }
+
+        // GET: api/Task/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TaskModel>> GetTaskModel(int id)
+        {
+            var taskModel = await _context.TaskModels.FindAsync(id);
+
+            if (taskModel == null)
+            {
+                return NotFound();
+            }
+
+            return taskModel;
+        }
+
+        // PUT: api/Task/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTaskModel(int id, TaskModel taskModel)
+        {
+            if (id != taskModel.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(taskModel).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TaskModelExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Task
+        [HttpPost]
+        public async Task<ActionResult<TaskModel>> PostTaskModel(TaskModel taskModel)
+        {
+            _context.TaskModels.Add(taskModel);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetTaskModel", new { id = taskModel.Id }, taskModel);
+        }
+
+        // DELETE: api/Task/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<TaskModel>> DeleteTaskModel(int id)
+        {
+            var taskModel = await _context.TaskModels.FindAsync(id);
+            if (taskModel == null)
+            {
+                return NotFound();
+            }
+
+            _context.TaskModels.Remove(taskModel);
+            await _context.SaveChangesAsync();
+
+            return taskModel;
+        }
+
+        private bool TaskModelExists(int id)
+        {
+            return _context.TaskModels.Any(e => e.Id == id);
+        }
+    }
+}
--- repool-server/repool-server/Startup.cs
+++ repool-server/repool-server/Startup.cs
@@ -14,6 +14,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json.Serialization;
 using repool_server.Models;
 
 namespace repool_server
@@ -33,7 +34,13 @@ namespace repool_server
             //Inject AppSettings
             services.Configure<ApplicationSettings>(Configuration.GetSection("ApplicationSettings"));
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc()
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_2)
+                .AddJsonOptions(options => {
+                    var resolver = options.SerializerSettings.ContractResolver;
+                    if (resolver != null)
+                        (resolver as DefaultContractResolver).NamingStrategy = null;
+                });
 
             services.AddDbContext<AuthenticationContext>(options =>
             options.UseSqlServer(Configuration.GetConnectionString("IdentityConnection")));
